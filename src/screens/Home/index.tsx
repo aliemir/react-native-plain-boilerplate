02ae87889c56b964d4b4b19217cf9c390b22bc10
Ali Emir Şen <senaliemir@gmail.com>
2020-11-15 19:21:45 +0300

    update example screen with image, svg and text

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,11 +1,29 @@
 import React from 'react';
-import { Text } from 'react-native';
 import SafeAreaView from 'react-native-safe-area-view';
 
+import { Box, Text, Image } from '@primitive';
+import { useTheme } from '@definitions/styled';
+import { Heart } from '@icons';
+
+import catWithFlowers from '@assets/images/cat-with-flower.jpg';
+
 export const Home = () => {
+  const theme = useTheme();
   return (
     <SafeAreaView>
-      <Text>Ali Emir</Text>
+      <Box>
+        <Text textAlign="center" pt="20px" fontSize={18} color={theme.colors.secondary}>
+          react-native-plain-boilerplate
+        </Text>
+      </Box>
+      <Box px="20px">
+        <Box py="20px" alignItems="center">
+          <Heart color="red" width={45} height={45} fill="pink" />
+        </Box>
+        <Box borderRadius={20} overflow="hidden">
+          <Image width="100%" height={200} resizeMode="cover" source={catWithFlowers} />
+        </Box>
+      </Box>
     </SafeAreaView>
   );
 };
